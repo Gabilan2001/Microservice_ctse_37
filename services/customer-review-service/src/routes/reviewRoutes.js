ff7f459f5702ffa7d50a8a980a922a7e455863bf
Rashadfaris <rashadfaris4675@gmail.com>
2026-03-20 22:52:23 +0530

    added route

--- services/customer-review-service/src/routes/reviewRoutes.js
+++ services/customer-review-service/src/routes/reviewRoutes.js
@@ -3,9 +3,10 @@ const express = require("express")
 const router = express.Router()
 
 const { createReview, getReviews, deleteReview } = require("../controllers/reviewController")
+const { protect, optionalProtect } = require("../middlewares/authMiddleware")
 
-router.post("/", createReview)
-router.get("/", getReviews)
-router.delete("/:id", deleteReview)
+router.post("/", protect, createReview)
+router.get("/", optionalProtect, getReviews)
+router.delete("/:id", protect, deleteReview)
 
 module.exports = router
