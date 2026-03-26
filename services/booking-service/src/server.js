b8e71c6c23df52f19e52972cdb33f1cad4525b0d
Gabilan2001 <IT22060426@my.sliit.lk>
2026-03-26 18:35:51 +0530

    ci cd testing

--- services/booking-service/src/server.js
+++ services/booking-service/src/server.js
@@ -13,6 +13,13 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 
+// Log every request
+app.use((req, res, next) => {
+  console.log('booking service working correctly')
+  next()
+})
+
+
 app.use("/api/bookings", bookingRoutes)
 
 const PORT = process.env.PORT || 3003
